fc874533e9c0bec9256b066ef971307fce739476
Kevin Raddatz <kevin@familie-raddatz.de>
2019-05-13 17:47:03 +0200

    feat(verified_email): implement verified email annotation

--- src/main/java/me/raddatz/jwarden/common/annotation/verifiedemail/VerifiedEmailValidator.java
+++ src/main/java/me/raddatz/jwarden/common/annotation/verifiedemail/VerifiedEmailValidator.java
@@ -1,12 +1,28 @@
 package me.raddatz.jwarden.common.annotation.verifiedemail;
 
 import me.raddatz.jwarden.common.error.EmailNotVerifiedException;
+import me.raddatz.jwarden.common.error.UserNotFoundException;
+import me.raddatz.jwarden.common.service.RequestHelperService;
+import me.raddatz.jwarden.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 public class VerifiedEmailValidator {
 
+    @Autowired private RequestHelperService requestHelperService;
+    @Autowired private UserRepository userRepository;
+
     public boolean validate() {
-        throw new EmailNotVerifiedException();
+        var user = userRepository.findOneByEmail(requestHelperService.getUserName());
+        if (Objects.isNull(user)) {
+            throw new UserNotFoundException();
+        }
+        if (!user.getEmailVerified()) {
+            throw new EmailNotVerifiedException();
+        }
+        return true;
     }
 }
--- src/test/java/me/raddatz/jwarden/common/annotation/verifiedemail/VerifiedEmailValidatorTest.java
+++ src/test/java/me/raddatz/jwarden/common/annotation/verifiedemail/VerifiedEmailValidatorTest.java
@@ -1,13 +1,22 @@
 package me.raddatz.jwarden.common.annotation.verifiedemail;
 
 import me.raddatz.jwarden.common.error.EmailNotVerifiedException;
+import me.raddatz.jwarden.common.error.UserNotFoundException;
+import me.raddatz.jwarden.common.service.RequestHelperService;
+import me.raddatz.jwarden.user.model.User;
+import me.raddatz.jwarden.user.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(VerifiedEmailValidator.class)
@@ -15,9 +24,41 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 class VerifiedEmailValidatorTest {
 
     @Autowired private VerifiedEmailValidator verifiedEmailValidator;
+    @MockBean private RequestHelperService requestHelperService;
+    @MockBean private UserRepository userRepository;
+
+    private User createDefaultUser() {
+        var user = new User();
+        user.setEmailVerified(true);
+        return user;
+    }
 
     @Test
-    void validate_throwException() {
-        Assertions.assertThrows(EmailNotVerifiedException.class, () -> verifiedEmailValidator.validate());
+    void validate_whenUserExists_thenReturnTrue() {
+        var user = createDefaultUser();
+        when(requestHelperService.getUserName()).thenReturn("user@email.com");
+        when(userRepository.findOneByEmail("user@email.com")).thenReturn(user);
+
+        var result = verifiedEmailValidator.validate();
+
+        assertTrue(result);
+    }
+
+    @Test
+    void validate_whenUserNotExists_thenThrowException() {
+        when(requestHelperService.getUserName()).thenReturn("invaliduser@email.com");
+        when(userRepository.findOneByEmail("invaliduser@email.com")).thenReturn(null);
+
+        assertThrows(UserNotFoundException.class, () -> verifiedEmailValidator.validate());
+    }
+
+    @Test
+    void validate_whenUserEmailNotVerified_thenThrowException() {
+        var user = createDefaultUser();
+        user.setEmailVerified(false);
+        when(requestHelperService.getUserName()).thenReturn("invaliduser@email.com");
+        when(userRepository.findOneByEmail("invaliduser@email.com")).thenReturn(user);
+
+        assertThrows(EmailNotVerifiedException.class, () -> verifiedEmailValidator.validate());
     }
 }
