7a4a5c855754086220e2e8c09a6d997434352c3f
Kevin Raddatz <kevin@familie-raddatz.de>
2019-05-29 14:45:03 +0200

    feat(properties): externalize some properties

--- src/main/java/me/raddatz/yapam/config/YapamProperties.java
+++ src/main/java/me/raddatz/yapam/config/YapamProperties.java
@@ -3,7 +3,11 @@ package me.raddatz.yapam.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
+import javax.sql.DataSource;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
@@ -18,11 +22,12 @@ public class YapamProperties {
     private Duration registrationTimeout;
     private Security security;
     private Mail mail;
+    private Datasource datasource;
 
     @Getter
     @Setter
     public static class Security {
-        private Integer bcryptIterations;
+        private Integer bcryptIterations = 10;
         private Oauth oauth;
 
         @Getter
@@ -45,6 +50,26 @@ public class YapamProperties {
         private Charset defaultEncoding = StandardCharsets.UTF_8;
         private Map<String, String> properties = new HashMap<>();
         private String jndiName;
+    }
+
+    @Getter
+    @Setter
+    public static class Datasource {
+        private String username;
+        private String password;
+        private String url;
+        private String name;
+    }
+
+    @ConfigurationProperties(prefix = "yapam.datasource")
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .password(this.datasource.password)
+                .url(this.datasource.url)
+                .username(this.datasource.username)
+                .build();
     }
 
 }
